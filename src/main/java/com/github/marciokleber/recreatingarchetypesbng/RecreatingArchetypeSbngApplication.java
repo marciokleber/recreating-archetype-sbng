4718c33b2073beea36a69cf05d375e4458ee7ae5
Marcio Kleber Junior Lopes de Souza <marciokjls@gmail.com>
2024-10-13 20:02:44 -0300

    :tada: First commit - criação de um archetype modelo de aplicações webs usando spring e angular.

---
+++ src/main/java/com/github/marciokleber/recreatingarchetypesbng/RecreatingArchetypeSbngApplication.java
@@ -0,0 +1,13 @@
+package com.github.marciokleber.recreatingarchetypesbng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecreatingArchetypeSbngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecreatingArchetypeSbngApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/marciokleber/recreatingarchetypesbng/RecreatingArchetypeSbngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.marciokleber.recreatingarchetypesbng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecreatingArchetypeSbngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
